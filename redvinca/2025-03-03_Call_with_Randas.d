1bc4f8b00fcb634dc835a6b7bb304d743326e236
ramdasrrao <ramdas.rao@gmail.com>
2025-03-03 17:16:52 +0530

    Create 2025-03-03_Call_with_Randas.md

---
+++ redvinca/2025-03-03_Call_with_Randas.d
@@ -0,0 +1,11 @@
+* Participants: Uday, Dinesh, Ramdas
+  
+* Uday mentioned that there were 2 tracks:
+	* 1st track: MicroPro Geospatial RFP response:
+		* Here, MicroPro (Amit) has shared a broad scope of work, which feels as if these are the points that they are seeking Red Vinca's help on.
+		* Red Vinca has not yet received either the original tender document / link, or the response.
+		* Uday has spoken to MicroPro, and clarified that Red Vinca cannot be part of the response team at this late state. Red Vinca will only evaluate the response created by MicroPro against the tender requirements, and suggest any improvements / shortcomings.
+		
+	* 2nd track: MicroPro Open Source BI/Analytics stack POC:
+		* Ramdas to submit broad steps for the POC, and the effort estimates to Red Vinca.
+		* Red Vinca will propose the same with modifications (if any), along with the cost estimates to MicroPro. This will be a paid POC by MicroPro.
